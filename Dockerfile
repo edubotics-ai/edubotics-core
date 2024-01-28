b83cc65328bde50318a44bf068c44dd6a76151cb
XThomasBU <xthomas@bu.edu>
2024-01-27 19:44:08 -0500

    hf sync commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM python:3.9
+
+WORKDIR /code
+
+COPY ./requirements.txt /code/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+RUN pip install --no-cache-dir transformers==4.36.2 torch==2.1.2
+
+RUN pip install --upgrade --force-reinstall --no-cache-dir llama-cpp-python==0.2.32
+
+COPY . /code
+
+RUN ls -R
+
+# Change permissions to allow writing to the directory
+RUN chmod -R 777 /code
+
+# Create a logs directory and set permissions
+RUN mkdir /code/logs && chmod 777 /code/logs
+
+# Create a cache directory within the application's working directory
+RUN mkdir /.cache && chmod -R 777 /.cache
+
+RUN --mount=type=secret,id=HUGGINGFACEHUB_API_TOKEN,mode=0444,required=true 
+RUN --mount=type=secret,id=OPENAI_API_KEY,mode=0444,required=true 
+
+CMD ["chainlit", "run", "code/main.py", "--host", "0.0.0.0", "--port", "7860"]
--- code/modules/constants.py
+++ code/modules/constants.py
@@ -10,15 +10,15 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Prompt Templates
 
-prompt_template = """Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+# prompt_template = """Use the following pieces of information to answer the user's question.
+# If you don't know the answer, just say that you don't know.
 
-Context: {context}
-Question: {question}
+# Context: {context}
+# Question: {question}
 
-Only return the helpful answer below and nothing else.
-Helpful answer:
-"""
+# Only return the helpful answer below and nothing else.
+# Helpful answer:
+# """
 
 prompt_template_with_history = """Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -31,3 +31,27 @@ Question: {question}
 Only return the helpful answer below and nothing else.
 Helpful answer:
 """
+
+prompt_template = """
+<|im_start|>system
+Assistant is an intelligent chatbot designed to help students with questions regarding the course. Only answer questions using the context below and if you're not sure of an answer, you can say "I don't know". Always give a breif and concise answer to the question.
+
+Context:
+{context}
+<|im_end|>
+<|im_start|>user
+Question: Who is the instructor for this course?
+<|im_end|>
+<|im_start|>assistant
+The instructor for this course is Prof. Thomas Gardos.
+<|im_end|>
+<|im_start|>user
+Question: {question}
+<|im_end|>
+<|im_start|>assistant
+"""
+
+# Model Paths
+
+LLAMA_PATH = "storage/models/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
+MISTRAL_PATH = "storage/models/mistral-7b-v0.1.Q4_K_M.gguf"
