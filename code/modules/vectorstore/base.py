7a98bd31e448af14b7498e53e7d68aafb18fd9f0
XThomasBU <144237146+XThomasBU@users.noreply.github.com>
2024-06-17 20:09:22 -0400

    Added comments to the base classes

--- code/modules/vectorstore/base.py
+++ code/modules/vectorstore/base.py
@@ -1,17 +1,32 @@
+# template for vector store classes
+
+
 class VectorStoreBase:
     def __init__(self, config):
         self.config = config
 
     def _init_vector_db(self):
+        """
+        Creates a vector store object
+        """
         raise NotImplementedError
 
-    def create_database(self, database_name):
+    def create_database(self):
+        """
+        Populates the vector store with documents
+        """
         raise NotImplementedError
 
-    def load_database(self, database_name):
+    def load_database(self):
+        """
+        Loads the vector store from disk
+        """
         raise NotImplementedError
 
     def as_retriever(self):
+        """
+        Returns the vector store as a retriever
+        """
         raise NotImplementedError
 
     def __str__(self):
