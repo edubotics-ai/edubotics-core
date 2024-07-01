fc2cb23b16e909a10419bc549fcdc821d0992d60
XThomasBU <144237146+XThomasBU@users.noreply.github.com>
2024-06-30 21:51:19 -0400

    improvements, refactored chat

--- code/modules/chat_processor/chat_processor.py
+++ code/modules/chat_processor/chat_processor.py
@@ -2,12 +2,24 @@ from modules.chat_processor.literal_ai import LiteralaiChatProcessor
 
 
 class ChatProcessor:
-    def __init__(self, config, tags=None):
-        self.chat_processor_type = config["chat_logging"]["platform"]
-        self.logging = config["chat_logging"]["log_chat"]
-        self.tags = tags
+    def __init__(self, llm_tutor, tags=None):
+        self.llm_tutor = llm_tutor
+        self.config = self.llm_tutor.config
+        self.chat_processor_type = self.config["chat_logging"]["platform"]
+        self.logging = self.config["chat_logging"]["log_chat"]
+        self.user = self.llm_tutor.user
+        if tags is None:
+            self.tags = self._create_tags()
+        else:
+            self.tags = tags
         if self.logging:
             self._init_processor()
+
+    def _create_tags(self):
+        tags = []
+        tags.append(self.config["vectorstore"]["db_option"])
+        tags.append(self.config["llm_params"]["chat_profile"])
+        return tags
 
     def _init_processor(self):
         if self.chat_processor_type == "literalai":
@@ -23,8 +35,21 @@ class ChatProcessor:
         else:
             pass
 
-    async def rag(self, user_query: str, chain, cb):
+    async def rag(self, user_query: str, chain):
+        user_query_dict = {"input": user_query}
+        # Define the base configuration
+        config = {
+            "configurable": {
+                "user_id": self.user["user_id"],
+                "conversation_id": self.user["session_id"],
+                "memory_window": self.llm_tutor.config["llm_params"]["memory_window"],
+            }
+        }
+
+        # Process the user query using the appropriate method
         if self.logging:
-            return await self.processor.rag(user_query, chain, cb)
+            return await self.processor.rag(
+                user_query=user_query_dict, config=config, chain=chain
+            )
         else:
-            return await chain.acall(user_query, callbacks=[cb])
+            return chain.invoke(user_query=user_query_dict, config=config)
