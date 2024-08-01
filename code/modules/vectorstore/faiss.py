6581a7671b3e8a0c91fb17695b1fa2c429767c88
Farid Karimli <karimli.farid@icloud.com>
2024-07-31 23:16:36 -0700

    Chunking strategy start

--- code/modules/vectorstore/faiss.py
+++ code/modules/vectorstore/faiss.py
@@ -7,6 +7,10 @@ class FaissVectorStore(VectorStoreBase):
     def __init__(self, config):
         self.config = config
         self._init_vector_db()
+        self.local_path = os.path.join(self.config["vectorstore"]["db_path"],
+                                       "db_" + self.config["vectorstore"]["db_option"]
+                                       + "_" + self.config["vectorstore"]["model"]
+                                       + "_" + config["splitter_options"]["chunking_mode"])
 
     def _init_vector_db(self):
         self.faiss = FAISS(
@@ -18,24 +22,12 @@ class FaissVectorStore(VectorStoreBase):
             documents=document_chunks, embedding=embedding_model
         )
         self.vectorstore.save_local(
-            os.path.join(
-                self.config["vectorstore"]["db_path"],
-                "db_"
-                + self.config["vectorstore"]["db_option"]
-                + "_"
-                + self.config["vectorstore"]["model"],
-            )
+            self.local_path
         )
 
     def load_database(self, embedding_model):
         self.vectorstore = self.faiss.load_local(
-            os.path.join(
-                self.config["vectorstore"]["db_path"],
-                "db_"
-                + self.config["vectorstore"]["db_option"]
-                + "_"
-                + self.config["vectorstore"]["model"],
-            ),
+            self.local_path,
             embedding_model,
             allow_dangerous_deserialization=True,
         )
