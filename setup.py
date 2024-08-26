7712b5231dc99218b7d5d8c04d9a6eb7553f6cb7
XThomasBU <144237146+XThomasBU@users.noreply.github.com>
2024-08-26 17:08:37 -0400

    to add requirements.txt to pip install

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the contents of requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="modules",
     version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.7",
+    install_requires=requirements,
     description="A Deep Learning for Data Science Tutor application",
 )
