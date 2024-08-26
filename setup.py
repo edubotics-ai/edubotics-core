8503d667dc90c2a92ad508318a01d9c840cc721b
XThomasBU <144237146+XThomasBU@users.noreply.github.com>
2024-08-26 19:25:21 -0400

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="modules",
+    name="dl4ds_tutor",
     version="0.1.0",
     packages=find_packages(where="modules"),
     package_dir={"": "modules"},
