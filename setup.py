68115199392c99156117a5ca89fc9d745142a9c2
XThomasBU <144237146+XThomasBU@users.noreply.github.com>
2024-08-26 17:08:55 -0400

    format change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Read the contents of requirements.txt
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
