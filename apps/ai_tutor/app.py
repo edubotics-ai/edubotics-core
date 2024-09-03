7cae28a7901279610a3bc5dee9aaf5aaa3ff4e8b
XThomasBU <144237146+XThomasBU@users.noreply.github.com>
2024-09-02 21:36:02 -0400

    commit for adding session databases (in-memory)

--- apps/ai_tutor/app.py
+++ apps/ai_tutor/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Response, HTTPException
+from fastapi import FastAPI, Request, Response, HTTPException, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from google.oauth2 import id_token
@@ -24,11 +24,13 @@ from helpers import (
 from edubotics_core.chat_processor.helpers import get_user_details, update_user_info
 from config.config_manager import config_manager
 import hashlib
+from typing import Optional
+from database import DatabaseFactory, Database
 
-# set config
+# Set up configuration
 config = config_manager.get_config().dict()
 
-# set constants
+# Set constants
 GITHUB_REPO = config["misc"]["github_repo"]
 DOCS_WEBSITE = config["misc"]["docs_website"]
 ALL_TIME_TOKENS_ALLOCATED = config["token_config"]["all_time_tokens_allocated"]
@@ -40,21 +42,28 @@ GOOGLE_CLIENT_ID = OAUTH_GOOGLE_CLIENT_ID
 GOOGLE_CLIENT_SECRET = OAUTH_GOOGLE_CLIENT_SECRET
 GOOGLE_REDIRECT_URI = f"{CHAINLIT_URL}/auth/oauth/google/callback"
 
+# Initialize FastAPI app
 app = FastAPI()
+
+# Serve static files
 app.mount("/public", StaticFiles(directory="public"), name="public")
+
+# Set CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Update with appropriate origins
+    allow_origins=["*"],
     allow_methods=["*"],
-    allow_headers=["*"],  # or specify the headers you want to allow
-    expose_headers=["X-User-Info"],  # Expose the custom header
+    allow_headers=["*"],
+    expose_headers=["X-User-Info"],
 )
 
+# Initialize Jinja2 templates
 templates = Jinja2Templates(directory="templates")
-session_store = {}
+
+# Set the Chainlit path
 CHAINLIT_PATH = "/chainlit_tutor"
 
-# only admin is given any additional permissions for now -- no limits on tokens
+# Load user roles from an encrypted JSON file
 with open("public/files/students_encrypted.json", "r") as file:
     USER_ROLES = json.load(file)
 
@@ -84,12 +93,9 @@ flow = Flow.from_client_config(
 
 
 def get_user_role(username: str):
-
-    # Function to deterministically hash emails
     def deterministic_hash(email, salt):
         return hashlib.pbkdf2_hmac("sha256", email.encode(), salt, 100000).hex()
 
-    # encrypt email (#FIXME: this is not the best way to do this, not really encryption, more like a hash)
     encryption_salt = EMAIL_ENCRYPTION_KEY.encode()
     encrypted_email = deterministic_hash(username, encryption_salt)
     role = USER_ROLES.get(encrypted_email, ["guest"])
@@ -100,7 +106,15 @@ def get_user_role(username: str):
     return role
 
 
-async def get_user_info_from_cookie(request: Request):
+# get DB instance
+def get_db() -> Database:
+    db_type = config.get("database_type", "sqlite")  # Default to SQLite
+    print(f"Using database type: {db_type}")
+    return DatabaseFactory.create_database(db_type)
+
+
+# Get user info from cookie
+async def get_user_info_from_cookie(request: Request) -> Optional[dict]:
     user_info_encoded = request.cookies.get("X-User-Info")
     if user_info_encoded:
         try:
@@ -112,24 +126,32 @@ async def get_user_info_from_cookie(request: Request):
     return None
 
 
-async def del_user_info_from_cookie(request: Request, response: Response):
-    # Delete cookies from the response
+# Delete user info from cookie and session store
+async def del_user_info_from_cookie(
+    request: Request, response: Response, db: Database = Depends(get_db)
+):
     response.delete_cookie("X-User-Info")
     response.delete_cookie("session_token")
-    # Get the session token from the request cookies
     session_token = request.cookies.get("session_token")
-    # Check if the session token exists in the session_store before deleting
-    if session_token and session_token in session_store:
-        del session_store[session_token]
+    if session_token:
+        session = db.create_session()
+        db.delete(session, session_token)
+        db.commit(session)
+        db.close(session)
 
 
-def get_user_info(request: Request):
+# Get user info from session store
+def get_user_info(request: Request, db: Database = Depends(get_db)) -> Optional[dict]:
     session_token = request.cookies.get("session_token")
-    if session_token and session_token in session_store:
-        return session_store[session_token]
+    if session_token:
+        session = db.create_session()
+        user_data = db.get(session, session_token)
+        db.close(session)
+        return user_data
     return None
 
 
+# Login page route
 @app.get("/", response_class=HTMLResponse)
 async def login_page(request: Request):
     user_info = await get_user_info_from_cookie(request)
@@ -141,42 +163,20 @@ async def login_page(request: Request):
     )
 
 
-# @app.get("/login/guest")
-# async def login_guest():
-#     username = "guest"
-#     session_token = secrets.token_hex(16)
-#     unique_session_id = secrets.token_hex(8)
-#     username = f"{username}_{unique_session_id}"
-#     session_store[session_token] = {
-#         "email": username,
-#         "name": "Guest",
-#         "profile_image": "",
-#         "google_signed_in": False,  # Ensure guest users do not have this flag
-#     }
-#     user_info_json = json.dumps(session_store[session_token])
-#     user_info_encoded = base64.b64encode(user_info_json.encode()).decode()
-
-#     # Set cookies
-#     response = RedirectResponse(url="/post-signin", status_code=303)
-#     response.set_cookie(key="session_token", value=session_token)
-#     response.set_cookie(key="X-User-Info", value=user_info_encoded, httponly=True)
-#     return response
-
-
+# Unauthorized route
 @app.get("/unauthorized", response_class=HTMLResponse)
 async def unauthorized(request: Request):
     return templates.TemplateResponse("unauthorized.html", {"request": request})
 
 
+# Google login route
 @app.get("/login/google")
 async def login_google(request: Request):
-    # Clear any existing session cookies to avoid conflicts with guest sessions
     response = RedirectResponse(url="/post-signin")
     response.delete_cookie(key="session_token")
     response.delete_cookie(key="X-User-Info")
 
     user_info = await get_user_info_from_cookie(request)
-    # Check if user is already signed in using Google
     if user_info and user_info.get("google_signed_in"):
         return RedirectResponse("/post-signin")
     else:
@@ -184,8 +184,9 @@ async def login_google(request: Request):
         return RedirectResponse(authorization_url, headers=response.headers)
 
 
+# Google OAuth callback route
 @app.get("/auth/oauth/google/callback")
-async def auth_google(request: Request):
+async def auth_google(request: Request, db: Database = Depends(get_db)):
     try:
         flow.fetch_token(code=request.query_params.get("code"))
         credentials = flow.credentials
@@ -202,50 +203,52 @@ async def auth_google(request: Request):
             return RedirectResponse("/unauthorized")
 
         session_token = secrets.token_hex(16)
-        session_store[session_token] = {
+        literalai_user = await get_user_details(email)
+
+        session_data = {
+            "session_token": session_token,
             "email": email,
             "name": name,
             "profile_image": profile_image,
-            "google_signed_in": True,  # Set this flag to True for Google-signed users
+            "google_signed_in": True,
+            "literalai_info": literalai_user.to_dict(),
         }
 
-        # add literalai user info to session store to be sent to chainlit
-        literalai_user = await get_user_details(email)
-        session_store[session_token]["literalai_info"] = literalai_user.to_dict()
-        session_store[session_token]["literalai_info"]["metadata"]["role"] = role
+        session = db.create_session()
+        db.add(session, session_data)
+        db.commit(session)
+        db.close(session)
 
-        user_info_json = json.dumps(session_store[session_token])
+        user_info_json = json.dumps(session_data)
         user_info_encoded = base64.b64encode(user_info_json.encode()).decode()
 
-        # Set cookies
         response = RedirectResponse(url="/post-signin", status_code=303)
-        response.set_cookie(key="session_token", value=session_token)
-        response.set_cookie(
-            key="X-User-Info", value=user_info_encoded, httponly=True
-        )  # TODO: is the flag httponly=True necessary?
+        response.set_cookie(key="session_token", value=session_token, httponly=True)
+        response.set_cookie(key="X-User-Info", value=user_info_encoded, httponly=True)
         return response
     except Exception as e:
         print(f"Error during Google OAuth callback: {e}")
         return RedirectResponse(url="/", status_code=302)
 
 
-@app.get("/cooldown")
-async def cooldown(request: Request):
+# Cooldown route
+@app.get("/cooldown", response_class=HTMLResponse)
+async def cooldown(request: Request, db: Database = Depends(get_db)):
     user_info = await get_user_info_from_cookie(request)
     user_details = await get_user_details(user_info["email"])
     current_datetime = get_time()
+    user_role = get_user_role(user_info["email"])
     cooldown, cooldown_end_time = await check_user_cooldown(
         user_details, current_datetime, COOLDOWN_TIME, TOKENS_LEFT, REGEN_TIME
     )
-    print(f"User in cooldown: {cooldown}")
-    print(f"Cooldown end time: {cooldown_end_time}")
-    if cooldown and "admin" not in get_user_role(user_info["email"]):
+
+    if cooldown and "admin" not in user_role:
         return templates.TemplateResponse(
             "cooldown.html",
             {
                 "request": request,
                 "username": user_info["email"],
-                "role": get_user_role(user_info["email"]),
+                "role": user_role,
                 "cooldown_end_time": cooldown_end_time,
                 "tokens_left": user_details.metadata["tokens_left"],
             },
@@ -261,30 +264,29 @@ async def cooldown(request: Request):
         return RedirectResponse("/post-signin")
 
 
+# Post-signin route
 @app.get("/post-signin", response_class=HTMLResponse)
-async def post_signin(request: Request):
+async def post_signin(request: Request, db: Database = Depends(get_db)):
     user_info = await get_user_info_from_cookie(request)
     if not user_info:
-        user_info = get_user_info(request)
+        user_info = get_user_info(request, db)
     user_details = await get_user_details(user_info["email"])
     current_datetime = get_time()
+    user_role = get_user_role(user_info["email"])
     user_details.metadata["last_login"] = current_datetime
-    # if new user, set the number of tries
+
     if "tokens_left" not in user_details.metadata:
-        user_details.metadata["tokens_left"] = (
-            TOKENS_LEFT  # set the number of tokens left for the new user
-        )
+        user_details.metadata["tokens_left"] = TOKENS_LEFT
     if "last_message_time" not in user_details.metadata:
         user_details.metadata["last_message_time"] = current_datetime
     if "all_time_tokens_allocated" not in user_details.metadata:
         user_details.metadata["all_time_tokens_allocated"] = ALL_TIME_TOKENS_ALLOCATED
     if "in_cooldown" not in user_details.metadata:
         user_details.metadata["in_cooldown"] = False
+
     await update_user_info(user_details)
 
-    if "last_message_time" in user_details.metadata and "admin" not in get_user_role(
-        user_info["email"]
-    ):
+    if "last_message_time" in user_details.metadata and "admin" not in user_role:
         cooldown, _ = await check_user_cooldown(
             user_details, current_datetime, COOLDOWN_TIME, TOKENS_LEFT, REGEN_TIME
         )
@@ -301,14 +303,13 @@ async def post_signin(request: Request):
 
     if user_info:
         username = user_info["email"]
-        role = get_user_role(username)
         jwt_token = request.cookies.get("X-User-Info")
         return templates.TemplateResponse(
             "dashboard.html",
             {
                 "request": request,
                 "username": username,
-                "role": role,
+                "role": user_role,
                 "jwt_token": jwt_token,
                 "tokens_left": user_details.metadata["tokens_left"],
                 "all_time_tokens_allocated": user_details.metadata[
@@ -320,9 +321,10 @@ async def post_signin(request: Request):
     return RedirectResponse("/")
 
 
+# Start tutor route
 @app.get("/start-tutor")
 @app.post("/start-tutor")
-async def start_tutor(request: Request):
+async def start_tutor(request: Request, db: Database = Depends(get_db)):
     user_info = await get_user_info_from_cookie(request)
     if user_info:
         user_info_json = json.dumps(user_info)
@@ -335,6 +337,7 @@ async def start_tutor(request: Request):
     return RedirectResponse(url="/")
 
 
+# HTTP exception handler
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request: Request, exc: HTTPException):
     if exc.status_code == 404:
@@ -348,6 +351,7 @@ async def http_exception_handler(request: Request, exc: HTTPException):
     )
 
 
+# General exception handler
 @app.exception_handler(Exception)
 async def exception_handler(request: Request, exc: Exception):
     return templates.TemplateResponse(
@@ -355,18 +359,19 @@ async def exception_handler(request: Request, exc: Exception):
     )
 
 
+# Logout route
 @app.get("/logout", response_class=HTMLResponse)
-async def logout(request: Request, response: Response):
-    await del_user_info_from_cookie(request=request, response=response)
+async def logout(request: Request, response: Response, db: Database = Depends(get_db)):
+    await del_user_info_from_cookie(request=request, response=response, db=db)
     response = RedirectResponse(url="/", status_code=302)
-    # Set cookies to empty values and expire them immediately
     response.set_cookie(key="session_token", value="", expires=0)
     response.set_cookie(key="X-User-Info", value="", expires=0)
     return response
 
 
+# Get tokens left route
 @app.get("/get-tokens-left")
-async def get_tokens_left(request: Request):
+async def get_tokens_left(request: Request, db: Database = Depends(get_db)):
     try:
         user_info = await get_user_info_from_cookie(request)
         user_details = await get_user_details(user_info["email"])
@@ -382,6 +387,7 @@ async def get_tokens_left(request: Request):
         return {"tokens_left": 0}
 
 
+# Mount Chainlit
 mount_chainlit(app=app, target="chainlit_app.py", path=CHAINLIT_PATH)
 
 if __name__ == "__main__":
